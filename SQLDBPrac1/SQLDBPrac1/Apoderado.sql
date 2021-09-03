8e5c0e8cc2172c48a8c64ef97fcc1c9460b09dfa
Jair <jair.leo31@gmail.com>
2021-09-02 21:54:16 -0500

    Segunda Parte
    Clase2

--- SQLDBPrac1/SQLDBPrac1/Apoderado.sql
+++ SQLDBPrac1/SQLDBPrac1/Apoderado.sql
@@ -3,5 +3,6 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Nombre] NVARCHAR(50) NOT NULL, 
     [Telefono] NVARCHAR(50) NOT NULL, 
-    [ID_Alumno] INT NOT NULL
+    [ID_Alumno] INT NOT NULL, 
+    CONSTRAINT [FK_Apoderado_ToTable] FOREIGN KEY ([ID_Alumno]) REFERENCES [Alumno]([Id])
 )
--- SQLDBPrac1/SQLDBPrac1/CURSO.sql
+++ SQLDBPrac1/SQLDBPrac1/CURSO.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[CURSO]
 (
 	[cod] INT NOT NULL PRIMARY KEY, 
-    [Nombre] NCHAR(10) NULL, 
+    [Nombre] NVARCHAR(50) NULL, 
     [Fecha_Inicio] NVARCHAR(50) NULL,
     [duracion] INT NULL, 
     [valor] INT NULL,
