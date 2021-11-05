ad0391bd3b0d2a1cf4bdec49e22174257ae0612c
Jair <jair.leo31@gmail.com>
2021-11-04 21:46:08 -0500

    BDMercamax

---
+++ BDMercamax/BDMercamax/Personal.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Personal]
+(
+	[cc_personal] INT NOT NULL PRIMARY KEY, 
+    [nombre_apellido_personal] NVARCHAR(MAX) NULL, 
+    [cargo] BIT NULL, 
+    [telefono_personal] NVARCHAR(MAX) NULL, 
+    [email_personal] NVARCHAR(MAX) NULL, 
+    [direccion_personal] NVARCHAR(MAX) NULL, 
+    [fecha_nacimiento_personal] DATE NULL, 
+    [password] NCHAR(10) NULL
+)
